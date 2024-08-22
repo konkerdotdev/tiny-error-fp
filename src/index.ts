08877564e613a378221c088b975a569523c7f1ea
Konrad Markus <konker@iki.fi>
2024-08-22 20:07:26 +0300

    Add some helper functions

--- src/index.ts
+++ src/index.ts
@@ -13,3 +13,18 @@ export const toTinyError =
       cause: x,
     };
   };
+
+export const isTinyError =
+  <T extends string>(tag: T) =>
+  (x: unknown): x is TinyError<T> =>
+    // Check that has matching _tag and a message
+    !!x && typeof x === 'object' && '_tag' in x && x._tag === tag && 'message' in x && typeof x.message === 'string';
+
+// --------------------------------------------------------------------------
+export function hasErrorMessage(x: unknown): boolean {
+  return typeof x === 'object' && !!x && 'message' in x && typeof x.message === 'string';
+}
+
+export function toError(x: unknown): Error {
+  return x instanceof Error ? x : new Error(String(x));
+}
