46fe47330f9b14a251d34cc422cc691e1f488118
Konrad Markus <konker@iki.fi>
2024-08-27 13:28:55 +0300

    Add toObject

--- src/consts.ts
+++ src/consts.ts
@@ -3,4 +3,4 @@ export const TINY_ERROR_DEFAULT_CODE_TAG = 'GENERAL';
 export const TINY_ERROR_DEFAULT_INTERNAL = true;
 
 export const TINY_ERROR_UNKNOWN_STRING = () => 'UNKNOWN';
-export const TINY_ERROR_DEFAULT_STACK = 'No stack trace available';
+export const TINY_ERROR_UNKNOWN_STACK = 'No stack trace available';
